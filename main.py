bf88ad5f5d8694a4a34a0a373ee811b3744e75e8
JaeHyunEo <jaehyuneo@gmail.com>
2024-11-06 14:45:40 +0900

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-
+import psutil
+import subprocess
 import streamlit as st
 
 work_dir = os.path.dirname(os.path.abspath((__file__)))
@@ -11,7 +12,7 @@ def _set_pages():
         "app_pages/dashboard.py", title="Dashboard", icon=":material/dashboard:", default=True
     )
     prompts = st.Page(
-        "app_pages/prompts.py", title="Prompts", icon=":material/Robot:"
+        "app_pages/prompts.py", title="Prompts", icon=":material/robot:"
     )
     page_1 = st.Page("app_pages/generate_stream.py", title="광고 생성 봇", icon=":material/search:")
     page_2 = st.Page("app_pages/review_stream.py", title="광고 심의 봇", icon=":material/history:")
@@ -25,7 +26,27 @@ def _set_pages():
     pg.run()
 
 
+def is_redis_running():
+    """Check if Redis server is running."""
+    for process in psutil.process_iter(['name', 'cmdline']):
+        if 'redis-server' in process.info['name'] or any('redis-server' in arg for arg in process.info['cmdline']):
+            return True
+    return False
+
+
+def start_redis():
+    """Start Redis server if not already running and if OS is not Windows."""
+    if os.name == 'posix':  # UNIX 계열 운영 체제 확인
+        if not is_redis_running():
+            print("Starting Redis server...")
+            subprocess.run(["redis-server"])
+        else:
+            print("Redis server is already running.")
+    else:
+        print("This code is only for UNIX-based systems.")
+
+
 if __name__ == "__main__":
-    # subprocess.run(["redis-server"])
+    start_redis()
     _set_pages()
     # redis.startup()
