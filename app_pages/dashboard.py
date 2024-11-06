65e346e2d257ee0baf899eb1d3b7cdc5e9d8b6fa
JaeHyunEo <jaehyuneo@gmail.com>
2024-11-06 18:31:42 +0900

    update

--- app_pages/dashboard.py
+++ app_pages/dashboard.py
@@ -1,7 +1,92 @@
-import streamlit as st
-import pandas as pd
-from langchain_core.prompts import load_prompt
-import numpy as np
-import os
+import sqlite3
+from datetime import datetime, timedelta
 
-st.title("📊 dashboard")
+import streamlit as st
+from streamlit_echarts import st_echarts
+
+st.title("📊 Dashboard")
+
+# 날짜 범위 설정
+today = datetime.today()
+a_week_ago = today - timedelta(days=7)
+
+# 날짜 선택 입력
+date_inputs = st.date_input(
+    "조회 날짜를 선택해 주세요.",
+    (a_week_ago, today),
+    min_value=a_week_ago,
+    max_value=today,
+    format="YYYY-MM-DD",
+)
+
+
+# 주어진 날짜 범위의 날짜 목록 생성
+def generate_date_range(start_date, end_date, date_format="%Y-%m-%d"):
+    return [(start_date + timedelta(days=i)).strftime(date_format)
+            for i in range((end_date - start_date).days + 1)]
+
+
+category_days = generate_date_range(date_inputs[0], date_inputs[1])
+short_category_days = generate_date_range(date_inputs[0], date_inputs[1], date_format="%m-%d")
+
+# 조회 기간 설정
+start_date_str, end_date_str = category_days[0], category_days[-1]
+end_date_str = (today + timedelta(days=1)).strftime('%Y-%m-%d') if today.strftime('%Y-%m-%d') == end_date_str else end_date_str
+
+# 초기값 설정
+token_dict = dict.fromkeys(category_days, 0)
+cost_dict = dict.fromkeys(category_days, 0)
+
+# SQLite 데이터베이스 연결 및 데이터 가져오기
+conn = sqlite3.connect(r'C:\Users\user\PycharmProjects\MarketingAdvisor\shcard.db')
+cursor = conn.cursor()
+cursor.execute(f"""
+    SELECT DATE(INPUT_TIME) AS DATE,
+           SUM(USAGE_COST) * 100 AS TOTAL_USAGE_COST,
+           SUM(USAGE_TOKEN_COUNT) / 1000 AS TOTAL_USAGE_TOKEN_COUNT
+    FROM TB_TRACE
+    WHERE INPUT_TIME BETWEEN DATE('{start_date_str}') AND DATE('{end_date_str}')
+    GROUP BY DATE(INPUT_TIME)
+    ORDER BY DATE
+""")
+rows = cursor.fetchall()
+
+# 데이터 업데이트
+for date, total_usage_cost, total_usage_token_count in rows:
+    token_dict[date] = total_usage_token_count
+    cost_dict[date] = total_usage_cost
+
+# 그래프 옵션 설정
+options = {
+    "title": {"text": "사용 정보"},
+    "tooltip": {"trigger": "axis"},
+    "legend": {"data": ["토큰 사용량(k)", "사용 비용(¢)"]},
+    "grid": {"left": "3%", "right": "4%", "bottom": "3%", "containLabel": True},
+    "toolbox": {},
+    "xAxis": {
+        "type": "category",
+        "boundaryGap": True,
+        "data": short_category_days,
+    },
+    "yAxis": {"type": "value"},
+    "series": [
+        {
+            "name": "토큰 사용량(k)",
+            "type": "line",
+            "stack": "총량",
+            "data": list(token_dict.values()),
+        },
+        {
+            "name": "사용 비용(¢)",
+            "type": "bar",
+            "stack": "총량",
+            "data": list(cost_dict.values()),
+        },
+    ],
+}
+
+# 차트 렌더링
+st_echarts(options=options, height="400px")
+
+# 데이터베이스 연결 종료
+conn.close()
