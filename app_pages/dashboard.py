c8de7257943dc4fc51e14b64dd87a2024df52fa8
JaeHyunEo <jaehyuneo@gmail.com>
2024-11-07 11:58:04 +0900

    update

--- app_pages/dashboard.py
+++ app_pages/dashboard.py
@@ -120,5 +120,5 @@ df_h = subprocess.run(["df", "-h"], capture_output=True, text=True).stdout.split
 
 st.info(f"du_sh : {du_sh}")
 st.info(f"pwd : {pwd}")
-st.markdown(f"ls_al : {"<br>".join(ls_al)}", unsafe_allow_html=True)
+st.code(f"ls_al : {"<br>".join(ls_al)}", unsafe_allow_html=True)
 st.dataframe(parse_df_h(df_h))
